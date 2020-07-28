df85f6a747751d51403d26b4122b3a287266ed29
Kory Rogers <kmroge05@gmail.com>
2020-07-27 21:48:39 -0400

    added form validation functions

--- pt.js
+++ pt.js
@@ -1,3 +1,5 @@
+// Image Carousel 
+
 var slideIndex = 0;
 carousel();
 
@@ -12,3 +14,62 @@ function carousel() {
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 2800); // Change image every 2.8 seconds
 }
+
+// Form Validation
+
+// Name Validation Function
+function validateName() {
+  const name = document.getElementById('name').value;
+  const nameError = document.getElementById('name-error');
+
+
+  if (name === '' || name == null) {
+    nameError.innerText = "Please enter your name."; 
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Email Validation Function
+function validateEmail() {
+  const email = document.getElementById('email').value;
+  const emailError = document.getElementById('email-error');
+  
+  // Regular expession to validate email format
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if ( email.match(pattern) && email.length > 0 ) {
+    return true;
+  } else {
+    emailError.innerText = "Please enter a valid email." 
+    return false;
+  }
+}
+
+// Phone Validation Function
+function validatePhone() {
+  const phone = document.getElementById('phone').value;
+  const phoneError = document.getElementById('phone-error');
+
+  if (phone === '' || isNaN(phone) || phone.length != 10) {
+    phoneError.innerText = "Please enter a 10-digit phone number."
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
